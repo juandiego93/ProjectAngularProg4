6d7e1894e441bcb7809aabda3b50d453e56dc8bf
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-10-13 21:42:07 -0500

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { UserComponent } from './components/pages/user/user.component';
+import { GameComponent } from './components/pages/game/game.component';
+import { GamesComponent } from './components/pages/games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    GameComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
