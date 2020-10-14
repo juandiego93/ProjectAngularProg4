d1230ac34298fdc8f4776fa5cb51a925cc520ad4
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-10-14 00:13:48 -0500

    create templates auth, add images and css

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  getAnio = new Date().getFullYear()
+
   constructor() { }
 
   ngOnInit(): void {
