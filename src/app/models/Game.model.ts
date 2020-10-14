7bd65505a2c24a3c54dd37204ec5e11e2c54c40f
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-10-14 01:34:33 -0500

    Edit templates, add models

---
+++ src/app/models/Game.model.ts
@@ -0,0 +1,14 @@
+export class GameModel {
+    name: string;
+    desc: string;
+    type: string;
+    size: number;
+    price: number;
+    stars: number;
+    owner: string;
+    status: boolean;
+    img: string;
+    platform: string[];
+    specifications: string;
+    keywords: string[];
+}
---
+++ src/app/models/User.model.ts
@@ -0,0 +1,8 @@
+export class UserModel {
+    username: string;
+    names: string;
+    surnames: string;
+    email: string;
+    password: string;
+    confirm_password: string
+}
